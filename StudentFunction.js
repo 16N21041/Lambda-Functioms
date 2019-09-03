1f7d9e68ce7d15d205cbc7f488c7a3bdc576c57c
16N21041 <37608548+16N21041@users.noreply.github.com>
2019-09-03 07:14:27 +0530

    Add files via upload

---
+++ StudentFunction.js
@@ -0,0 +1,48 @@
+var aws = require('aws-sdk');
+var ddb = new aws.DynamoDB({
+    apiVersion: '2012-10-08'
+});
+exports.handler = async (event, context) => {
+    let date = new Date();
+    const tableName = process.env.TABLE_NAME;
+    const region = process.env.REGION;
+    aws.config.update({
+        region: region
+    });
+
+    //Checking if the required attribute i.e., sub (or unique id) is present in "Cognito User Pool" or not.
+    if (event.request.userAttributes.sub) {
+
+        // -- Write data to DDB i.e., StudentTable.
+        let ddbParams = {
+            TableName: tableName,
+            Item: {
+                'studentId': {
+                    S: event.request.userAttributes.sub
+                },
+                'studentEmail': {
+                    S: event.request.userAttributes.email
+                },
+                'createdDate': {
+                    S: date.toISOString()
+                }
+            }
+        };
+
+        // Calling DynamoDB.
+        try {
+            let ddbResult = await ddb.putItem(ddbParams).promise();
+            console.log("Success");
+        } catch (err) {
+            console.log("Error", err);
+        }
+
+        console.log("Success: Everything executed correctly")
+        context.done(null, event);
+
+    } else {
+        // The student's Email-ID is unknown.
+        console.log("Error: Nothing was written to DDB or SQS");
+        context.done(null, event);
+    }
+};
